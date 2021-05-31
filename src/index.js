213f930bb98d47c572821ae8f10f26f6f4fbe011
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2021-05-31 15:55:57 +0300

    32

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import './css/basicLightbox.min.css';
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-// import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial';
 
 const refs = {
   button: document.querySelector('.button'),
@@ -24,9 +23,9 @@ let pageNumber = 1;
 
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
-  //   const photoQuery = e.target[0].value;
+
   pageNumber = 1;
-  //   console.log(e.target[0].value);
+
   refs.gallery.innerHTML = '';
   if (refs.input.value === '' || refs.input.value === ' ' || refs.input.value === '  ') {
     return error({
@@ -36,23 +35,6 @@ refs.form.addEventListener('submit', e => {
   } else {
     fetchPhoto(refs.input.value, pageNumber).then(renderPhoto);
   }
-  if (refs.gallery !== 0) {
-    // fetchPhoto(refs.input.value, pageNumber).then(renderPhoto);
-    // console.log(refs.gallery.getElementsByTagName('li').lenght);
-  } else {
-    return error({
-      text: '404',
-      delay: 2000,
-    });
-  }
-
-  //   if (e.currentTarget.elements.query.value === '') {
-  //     PNotify.error({
-  //       text: '404 Not fount',
-  //     });
-  //   } else {
-  //   }
-  //   console.log(e.currentTarget.elements.query.value);
 });
 function renderPhoto(data) {
   refs.gallery.insertAdjacentHTML('beforeend', cardMarkup(data));
@@ -73,55 +55,3 @@ refs.gallery.addEventListener('click', e => {
   }
   console.log(e.target.dataset.source);
 });
-
-// refs.form.addEventListener('input', debounce(onSearchPhoto, 1000));
-
-// function onSearchPhoto({ target: { value } }) {
-//   if (value.trim() !== '') {
-//     fetchPhoto(value.trim()).then(data => console.log(data));
-//   }
-// }
-
-// function renderCard(hits) {
-//   refs.gallery.insertAdjacentHTML('beforeend', cardMarkup(hits));
-// }
-// function searchFormInput(e) {
-//   const searchQuery = e.target.value;
-//   console.log(searchQuery);
-//   if (searchQuery.length > 0) {
-//     fetchCountries(searchQuery).then(data => {});
-//   }
-// }
-
-// function insertListItem(items) {
-//   refs.list.insertAdjacentHTML('beforeend', items);
-//   console.log(items);
-// }
-
-// function buildCountriesList(items) {
-//   return countriesListTemplate(items);
-// }
-
-// function buildListMarkup(items) {
-//   return countryListTemplate(items);
-// }
-
-// function clearListItems() {
-//   refs.list.innerHTML = '';
-// }
-
-// console.log(fetchCountries());
-// refs.button.addEventListener('click', e => {
-//   console.log(e);
-// });
-
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '21764210-8d882ab68fe5176a0369b7247';
-
-// function getData() {
-//   return axios(
-//     `${BASE_URL}?image_type=photo&orientation=horizontal&q=''&page=1&per_page=12&key=${API_KEY}`,
-//   ).then(({ data }) => data);
-// }
-// // console.log(data);
-// console.log(getData());
